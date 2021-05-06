6be101ac22506209c1dd94124bcf0a3147ed6429
AlanM Franco <jefeson23@live.com>
2021-05-06 19:25:42 -0300

    :recycle: Refatorar o código]

--- src/test/java/atdd010/Biblioteca.java
+++ src/test/java/atdd010/Biblioteca.java
@@ -1,15 +1,18 @@
 package atdd010;
 
 import java.util.ArrayList; 
-import java.util.List; 
+import java.util.List;
 
 public class Biblioteca { 
-	private List<Livro> livros = new ArrayList<>(); 
+ private List<Livro> livros = new ArrayList<>(); 
  
-	public void add(Livro livro) {} 
+ 	public void add(Livro livro) { 
+ 		livros.add(livro); 
+ 	} 
  
-	public int size() { 
-		return 1; 
-	} 
+ 	public int size() { 
+ 		return livros.size(); 
+ 	} 
  
 }
+
