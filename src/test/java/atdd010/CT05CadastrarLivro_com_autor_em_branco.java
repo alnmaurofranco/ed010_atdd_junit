5ab6b4173f51c9b1444b62270e5ee92169c9865e
AlanM Franco <jefeson23@live.com>
2021-05-07 00:31:27 -0300

    :bug: faz o teste passar

--- src/test/java/atdd010/CT05CadastrarLivro_com_autor_em_branco.java
+++ src/test/java/atdd010/CT05CadastrarLivro_com_autor_em_branco.java
@@ -11,6 +11,6 @@ public class CT05CadastrarLivro_com_autor_em_branco {
 		Biblioteca biblioteca = new Biblioteca(); 
 	 	Livro umLivro = new Livro("12345", "Engenharia de Software", ""); 
 	 	biblioteca.add(umLivro); 
-	 	assertEquals("", biblioteca.empty());
+	 	assertEquals("", umLivro.autor);
 	}
 }
